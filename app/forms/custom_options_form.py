ec1087168bc4f24053414c5fc9ddb21fa2d418f1
William Ziller <willziller@gmail.com>
2024-04-02 11:50:31 -0500

    Node update

--- app/forms/custom_options_form.py
+++ app/forms/custom_options_form.py
@@ -1,6 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, IntegerField
-from wtforms.fields.core import BooleanField
+from wtforms import StringField, TextAreaField, IntegerField, BooleanField
 from wtforms.validators import DataRequired, ValidationError
 from app.models import Order
 
